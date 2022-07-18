dce02db07cbf56786e37a0cb2d2ed4081a50e6d2
luoxz <luoxz@akulaku.com>
2022-07-18 17:40:33 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,59 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Test from '@/components/Test'
 import Login from '@/components/Login'
-import AppIndex from '../components/home/AppIndex'
+import AppIndex from '@/components/home/AppIndex'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Default',
+      redirect: '/home',
+      component: Home
     },
-    {
-      path: '/test',
-      name: 'Test',
-      component: Test
-    },
+    // {
+    //   path: '/test',
+    //   name: 'Test',
+    //   component: Test
+    // },
     {
       path: '/login',
       name: 'Login',
       component: Login
     },
     {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
     }
+    // {
+    //   path: '/index',
+    //   name: 'AppIndex',
+    //   component: AppIndex,
+    //   meta: {
+    //     requireAuth: true
+    //   }
+    // }
   ]
 })
